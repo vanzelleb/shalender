2590aeed5da9801c8bc94034551f7c18d3cb6ecc
Henning Wandsleb <76180365+vanzelleb@users.noreply.github.com>
2022-01-22 22:33:04 +0100

    corrected error with supabase import

--- Button.svelte
+++ Button.svelte
@@ -1,6 +1,11 @@
 <script>
 	//import axios from "axios";
-	import createClient from "@supabase/supabase-js";
+	import { createClient } from "@supabase/supabase-js";
+
+	const supabaseUrl = "https://jumyoetcqqeqmferdklb.supabase.co";
+	const supabaseKey =
+	  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyODczODc0LCJleHAiOjE5NTg0NDk4NzR9.8cBgXmf2_gvPn1znBLz8Wp1_zPkjkhJQwkmBgaXOhhw";
+	const supabase = createClient(supabaseUrl, supabaseKey);
 
 	// Backend consists of Netlify functions that can access secret API keys
 	// const backend = axios.create({
@@ -9,15 +14,10 @@
 
 	async function createEvent() {
 	  /* 	  const { data, error } = await backend.get("addEvent", {
-		    params: { name: "test", date: "2022-01-22" }
-		  });
-		  console.log("Data: ", data);
-			console.log("Error: ", error); */
-
-	  const supabase = createClient(
-	    "https://jumyoetcqqeqmferdklb.supabase.co",
-	    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyODczODc0LCJleHAiOjE5NTg0NDk4NzR9.8cBgXmf2_gvPn1znBLz8Wp1_zPkjkhJQwkmBgaXOhhw"
-	  );
+			    params: { name: "test", date: "2022-01-22" }
+			  });
+			  console.log("Data: ", data);
+				console.log("Error: ", error); */
 
 	  // Insert a row
 	  const { data, error } = await supabase
